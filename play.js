dd3faf127df5b1b1c35e4494d3b5892f08e6e917
nexus122 <juan-pablo122@hotmail.es>
2022-03-18 19:56:42 +0100

    Cambios generales

--- play.js
+++ play.js
@@ -1,102 +1,109 @@
 /* Primer intento, funciona pero nos complicamos... */
-
 // Creamos el estado de la aplicacion con los dias, horas, minutis y segundos.
+
 let state = {
     days: 0,
-    hours: 0,
+    hours: 1,
     minutes: 0,
-    seconds: 3,
-    daysBox: document.querySelector(".timer--days .timer--box"),
-    hoursBox: document.querySelector(".timer--hours .timer--box"),
-    minutesBox: document.querySelector(".timer--minutes .timer--box"),
-    secondsBox: document.querySelector(".timer--seconds span"),      
+    seconds: 0,
+    daysBox: document.querySelector(".timer--days .timer--box--bottom"),
+    hoursBox: document.querySelector(".timer--hours .timer--box--bottom"),
+    minutesBox: document.querySelector(".timer--minutes .timer--box--bottom"),
+    secondsBox: document.querySelector(".timer--seconds .timer--box--bottom"),
 }
 
 //Queremos que cada segundo nos reste un segundo del tiempo total. Cuando los segundos llegan a 0 nos tiene que restar un minuto. Cuando mlos minutos llegan a 0 nos tiene que restar una hora,  cunado las horas llegan a 0 nos tiene que restar un dia y cunado las 4 variables llegan a cero terimina el contador.
- 
+
 // Funcion que nos inicia el contador
 function timer() {
     // Inicializamos las variables a sus valores correspondientes
-    state.daysBox.innerHTML = state.days;
-    state.hoursBox.innerHTML = state.hours;
-    state.minutesBox.innerHTML = state.minutes;
-    state.secondsBox.innerHTML = state.seconds;
+    dubleNumber(state.days, state.daysBox);
+    dubleNumber(state.hours, state.hoursBox);
+    dubleNumber(state.minutes, state.minutesBox);
+    dubleNumber(state.seconds, state.secondsBox);
 
     // Set interval de 1 segundo, modifica los valores del estado e imprime por pantalla los resultados
-    let timerBucle = setInterval(function() {
+    let timerBucle = setInterval(function () {
         // Si todos los valores estan a 0 paramos la aplicacion
-        if (state.days === 0 && state.hours === 0 && state.minutes === 0 && state.seconds === 0){
+        if (state.days === 0 && state.hours === 0 && state.minutes === 0 && state.seconds === 0) {
             clearInterval(timerBucle); // Para el set interval
             return;
-        }        
+        }
 
         // Si los segundos estan a 0 restamos 1 a minutos y ponemos segundos a 59        
-        if (state.seconds == 0 && state.minutes >= 1){
-            console.log("Entramos en segundos")
+        if (state.seconds == 0 && state.minutes >= 1) {            
             state.minutes--;
-            state.seconds = 60;
-            console.log("minutos: " + state.minutes);
+            state.seconds = 60;            
 
             // Modificamos el DOM
-            state.minutesBox.innerHTML = state.minutes;
-        }else if(state.seconds == 0){
-            console.log("Entramos en el else de segundos")
+            dubleNumber(state.minutes, state.minutesBox);
+        } else if (state.seconds == 0) {            
+
             state.seconds = 60;
 
-            if(state.hours >= 1 || state.days >= 1){
+            if (state.hours >= 1 || state.days >= 1) {
                 state.minutes = 59;
-                if(state.hours >= 1){
+                if (state.hours >= 1) {
                     state.hours--
-                    state.hoursBox.innerHTML = state.hours;
+                    dubleNumber(state.hours, state.hoursBox);
                 }
             }
 
             state.minutesBox.innerHTML = state.minutes;
-            
-            
+
+
         }
 
         // Si los minutos estan a 0 restamos 1 a horas y ponemos minutos a 59
-        if (state.minutes == 0 && state.hours >= 1){
-            console.log("Entramos en minutos")
+        if (state.minutes == 0 && state.hours >= 1) {            
             state.hours--;
-            state.minutes = 59;
-            console.log("minutos: " + state.minutes);            
+            state.minutes = 59;            
 
             // Modificamos el DOM
-            state.minutesBox.innerHTML = state.minutes;
-            state.hoursBox.innerHTML = state.hours;
-        }else if(state.minutes == 0 && state.hours >= 1){
-            console.log("Entramos en el else de minutos");
+            dubleNumber(state.minutes, state.minutesBox);
+            dubleNumber(state.hours, state.hoursBox);
+        } else if (state.minutes == 0 && state.hours >= 1) {            
             state.minutes = 59;
-            state.minutesBox.innerHTML = state.minutes;
-            state.hoursBox.innerHTML = state.hours;
+            dubleNumber(state.minutes, state.minutesBox);
+            dubleNumber(state.hours, state.hoursBox);
         }
 
         // Si las horas estan a 0 restamos uno a dias y ponemos horas a 23.
-        if (state.hours == 0 && state.days >= 1){
+        if (state.hours == 0 && state.days >= 1) {
             state.days--;
             state.hours = 23;
             console.log("minutos: " + state.minutes);
 
             // Modificamos el DOM
-            state.daysBox.innerHTML = state.days;
-            state.minutesBox.innerHTML = state.minutes;
-            state.hoursBox.innerHTML = state.hours;
-        }else if(state.hours == 0 && state.minutes == 0 && state.seconds == 0){
+            dubleNumber(state.days, state.daysBox);
+            dubleNumber(state.hours, state.hoursBox);
+            dubleNumber(state.minutes, state.minutesBox);
+            dubleNumber(state.seconds, state.secondsBox);
+        } else if (state.hours == 0 && state.minutes == 0 && state.seconds == 0) {
             state.hours = 23;
             // Modificamos el DOM
-            state.daysBox.innerHTML = state.days;
-            state.minutesBox.innerHTML = state.minutes;
-            state.hoursBox.innerHTML = state.hours;
+            dubleNumber(state.days, state.daysBox);
+            dubleNumber(state.hours, state.hoursBox);
+            dubleNumber(state.minutes, state.minutesBox);
         }
 
-        state.seconds--;
-        console.log("segundos: ", state.seconds);                
-        state.secondsBox.innerHTML = state.seconds;
+        state.seconds--;        
+        dubleNumber(state.seconds, state.secondsBox);
+
+        document.title = `${state.days} ${state.hours}:${state.minutes}:${state.seconds}`
 
     }, 1000);
-    
- }
 
- timer();
+}
+
+timer();
+
+// Funcion para escribir un numero de dos cifras
+function dubleNumber(num, donde) {
+    // Comprobamos si el numero tiene menos de 2 digitos, en ese caso añadimos un 0 delante.
+    if (num.toString().length == 1) {
+        donde.innerHTML = "0" + num;
+    } else {
+        donde.innerHTML = num;
+    }
+}
--- play2.js
+++ play2.js
@@ -1,22 +1,37 @@
 /* A la segunda va la vencida! */
+
 // Creamos el estado de la aplicacion con los dias, horas, minutis y segundos.
 let state = {
-    days: 0,
+    days: 2,
     hours: 0,
     minutes: 0,
-    seconds: 5,
-    daysBox: document.querySelector(".timer--days .timer--box"), 
-    daysBox2: document.querySelector(".timer--days .timer--box2"),    
-    hoursBox: document.querySelector(".timer--hours .timer--box"),
-    hoursBox2: document.querySelector(".timer--hours .timer--box2"),
-    minutesBox: document.querySelector(".timer--minutes .timer--box"),
-    minutesBox2: document.querySelector(".timer--minutes .timer--box2"),
-    secondsBox: document.querySelector(".timer--seconds .timer--box"),
-    secondsBox2: document.querySelector(".timer--seconds .timer--box2"),
+    seconds: 0,
+    daysBox: document.querySelector(".timer--days .timer--box--front"),
+    daysBox2: document.querySelector(".timer--days .timer--box--back"),
+    hoursBox: document.querySelector(".timer--hours .timer--box--front"),
+    hoursBox2: document.querySelector(".timer--hours .timer--box--back"),
+    minutesBox: document.querySelector(".timer--minutes .timer--box--front"),
+    minutesBox2: document.querySelector(".timer--minutes .timer--box--back"),
+    secondsBox: document.querySelector(".timer--seconds .timer--box--front"),
+    secondsBox2: document.querySelector(".timer--seconds .timer--box--back"),
 }
 
-//Queremos que cada segundo nos reste un segundo del tiempo total. Cuando los segundos llegan a 0 nos tiene que restar un minuto. Cuando mlos minutos llegan a 0 nos tiene que restar una hora,  cunado las horas llegan a 0 nos tiene que restar un dia y cunado las 4 variables llegan a cero terimina el contador.
- 
+// Requisitos.
+
+//Queremos que cada segundo nos reste un segundo del tiempo total. 
+// CONTADOR DE TIEMPO
+// 1. Cuando los segundos llegan a 0 nos tiene que restar un minuto.
+// 2. Cuando los minutos llegan a 0 nos tiene que restar una hora,
+// 3. Cuando las horas llegan a 0 nos tiene que restar un dia
+// 4. Cuando las 4 variables llegan a cero terimina el contador. (Extra!) Soltar Confeti
+
+// DOBLE 0
+// 1. Queremos que nos escriba un doble 0 cuando solo haya un digito
+
+// ANIMACIÓN
+// 2. Debe estar animado, y cada vez que haya un cambio en el contador activarla.
+
+
 // Funcion que nos inicia el contador
 function timer() {
     // Inicializamos las variables a sus valores correspondientes
@@ -26,30 +41,30 @@ function timer() {
     dubleNumber(state.days, state.daysBox);
 
     // Set interval de 1 segundo, modifica los valores del estado e imprime por pantalla los resultados
-    let timerBucle = setInterval(function() {
+    let timerBucle = setInterval(function () {
         // Si todos los valores estan a 0 paramos la aplicacion
-        if (state.days === 0 && state.hours === 0 && state.minutes === 0 && state.seconds === 0){
+        if (state.days === 0 && state.hours === 0 && state.minutes === 0 && state.seconds === 0) {
             clearInterval(timerBucle); // Para el set interval
             document.querySelector(".confeti").style.display = "block"
             return;
         }
 
         // Si los segundos estan a 0 y los minutos son mayores o iguales a 1, restamos uno a minutos y reiniciamos segundos        
-        if(state.seconds == 0 && state.minutes >= 1){
+        if (state.seconds == 0 && state.minutes >= 1) {
             state.minutes--;
             animation(state.minutesBox, state.minutesBox2);
             state.seconds = 60;
         }
 
         // Si los minutos estan a 0 y las horas son mayores o iguales a 1, restamos 1 hora y reiniciamos minutos y segundos
-        if(state.minutes == 0 && state.hours >=1){
+        if (state.minutes == 0 && state.hours >= 1) {
             state.hours--;
             animation(state.hoursBox, state.hoursBox2);
             state.minutes = 59;
             state.seconds = 60;
         }
         // Si las horas estan a 0 y los dias son mayores o iguales a 1 restamos 1 dia, y reiniciamos horas, minutos y segundos.
-        if(state.hours == 0 && state.days >= 1){
+        if (state.hours == 0 && state.days >= 1) {
             state.days--;
             animation(state.daysBox, state.daysBox2);
             state.hours = 23;
@@ -58,9 +73,9 @@ function timer() {
         }
 
         // Siempre restamos 1 segundo
-        state.seconds--;        
-        
-        
+        state.seconds--;
+
+
         animation(state.secondsBox, state.secondsBox2);
 
         // Convocamos funcion dobleNumber.
@@ -70,36 +85,36 @@ function timer() {
         dubleNumber(state.minutes, state.minutesBox);
         dubleNumber(state.hours, state.hoursBox);
         dubleNumber(state.days, state.daysBox);
-        
+
     }, 1000);
-    
- }
 
- function dubleNumber(num, donde){
+}
+
+function dubleNumber(num, donde) {
     // Comprobamos si el numero tiene menos de 2 digitos, en ese caso añadimos un 0 delante.
-    if(num.toString().length == 1){        
-        donde.innerHTML = "0"+num;
-    }else{
+    if (num.toString().length == 1) {
+        donde.innerHTML = "0" + num;
+    } else {
         donde.innerHTML = num;
-    }    
- }
+    }
+}
 
- function animation(tarjeta1, tarjeta2){
-    if(tarjeta1.classList[1] == "animacion-out"){
+function animation(tarjeta1, tarjeta2) {
+    if (tarjeta1.classList[1] == "animacion-out") {
         tarjeta1.classList.add("animacion-in");
         tarjeta1.classList.remove("animacion-out");
 
         tarjeta2.classList.add("animacion-out");
-        tarjeta2.classList.remove("animacion-in");            
-        
-    }else{
+        tarjeta2.classList.remove("animacion-in");
+
+    } else {
         tarjeta2.classList.add("animacion-in");
         tarjeta2.classList.remove("animacion-out");
 
         tarjeta1.classList.add("animacion-out");
         tarjeta1.classList.remove("animacion-in");
     }
- }
+}
 
- // Iniciamos la funcion timer.
- timer();
+// Iniciamos la funcion timer.
+timer();
