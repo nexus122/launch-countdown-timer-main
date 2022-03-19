a93880b65cf392de30d196caa6f88da98821e9aa
nexus122 <juan-pablo122@hotmail.es>
2022-03-19 12:32:54 +0100

    Mejora en el contador, doy por perdida la animación

--- play.js
+++ play.js
@@ -3,13 +3,20 @@
 
 let state = {
     days: 0,
-    hours: 1,
+    hours: 0,
     minutes: 0,
     seconds: 0,
+    // Input Values:
+    days_Input: document.forms["timerConfig"].days,
+    hours_Input: document.forms["timerConfig"].hours,
+    minutes_Input: document.forms["timerConfig"].minutes,
+    seconds_Input: document.forms["timerConfig"].seconds,
+    // DOM Element
     daysBox: document.querySelector(".timer--days .timer--box--bottom"),
     hoursBox: document.querySelector(".timer--hours .timer--box--bottom"),
     minutesBox: document.querySelector(".timer--minutes .timer--box--bottom"),
     secondsBox: document.querySelector(".timer--seconds .timer--box--bottom"),
+    form: document.querySelector(".configTimer"),
 }
 
 //Queremos que cada segundo nos reste un segundo del tiempo total. Cuando los segundos llegan a 0 nos tiene que restar un minuto. Cuando mlos minutos llegan a 0 nos tiene que restar una hora,  cunado las horas llegan a 0 nos tiene que restar un dia y cunado las 4 variables llegan a cero terimina el contador.
@@ -25,25 +32,30 @@ function timer() {
     // Set interval de 1 segundo, modifica los valores del estado e imprime por pantalla los resultados
     let timerBucle = setInterval(function () {
         // Si todos los valores estan a 0 paramos la aplicacion
-        if (state.days === 0 && state.hours === 0 && state.minutes === 0 && state.seconds === 0) {
+        console.log(state.days, state.hours, state.minutes, state.seconds);
+        if (state.days == 0 && state.hours == 0 && state.minutes == 0 && state.seconds == 0) {
+            console.log("Entramos y acabamos");
             clearInterval(timerBucle); // Para el set interval
             return;
         }
 
         // Si los segundos estan a 0 restamos 1 a minutos y ponemos segundos a 59        
-        if (state.seconds == 0 && state.minutes >= 1) {            
+        if (state.seconds == 0 && state.minutes >= 1) {
+            console.log("Entramos en minutos");
             state.minutes--;
-            state.seconds = 60;            
+            state.seconds = 60;
 
             // Modificamos el DOM
             dubleNumber(state.minutes, state.minutesBox);
-        } else if (state.seconds == 0) {            
-
+        } else if (state.seconds == 0) {
+            console.log("Entramos en el else de minutos");
             state.seconds = 60;
 
             if (state.hours >= 1 || state.days >= 1) {
+                console.log("Entramos en el else de minutos en horas");
                 state.minutes = 59;
                 if (state.hours >= 1) {
+                    console.log("Entramos en el if de horas del else de minutos");
                     state.hours--
                     dubleNumber(state.hours, state.hoursBox);
                 }
@@ -71,8 +83,7 @@ function timer() {
         // Si las horas estan a 0 restamos uno a dias y ponemos horas a 23.
         if (state.hours == 0 && state.days >= 1) {
             state.days--;
-            state.hours = 23;
-            console.log("minutos: " + state.minutes);
+            state.hours = 23;            
 
             // Modificamos el DOM
             dubleNumber(state.days, state.daysBox);
@@ -88,7 +99,7 @@ function timer() {
         }
 
         state.seconds--;        
-        dubleNumber(state.seconds, state.secondsBox);
+        dubleNumber(state.seconds, state.secondsBox);        
 
         document.title = `${state.days} ${state.hours}:${state.minutes}:${state.seconds}`
 
@@ -96,14 +107,37 @@ function timer() {
 
 }
 
-timer();
-
 // Funcion para escribir un numero de dos cifras
 function dubleNumber(num, donde) {
     // Comprobamos si el numero tiene menos de 2 digitos, en ese caso añadimos un 0 delante.
     if (num.toString().length == 1) {
-        donde.innerHTML = "0" + num;
+        donde.innerHTML = "0" + num;        
     } else {
-        donde.innerHTML = num;
+        donde.innerHTML = num;        
     }
 }
+
+function ocultar
+
+document.addEventListener("submit",function(e){
+    e.preventDefault();
+    console.log("Se ha enviado la información");
+    state.form.style.display = "none";
+    // Inicializamos los valores del contador.
+    state.days = state.days_Input.value;
+    state.hours = state.hours_Input.value;
+    state.minutes = state.minutes_Input.value;
+    state.seconds = state.seconds_Input.value;
+
+    if(state.days == 0){
+        document.querySelector(".timer--days").style.display = "none"
+    }
+    if(state.days == 0 && state.hours == 0){
+        document.querySelector(".timer--hours").style.display = "none"
+    }
+    if(state.days == 0 && state.hours == 0 && state.minutes == 0){
+        document.querySelector(".timer--minutes").style.display = "none"
+    }
+
+    timer();
+});
